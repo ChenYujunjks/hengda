87632d1fa7a64d39127951fa2c71410e53469b25
bruce chen <aaaeeettt124@126.com>
2024-06-02 17:59:37 +0800

    new_target

--- delet_70.py
+++ delet_70.py
@@ -1,16 +1,14 @@
 from openpyxl import load_workbook, Workbook
 import os
 
-# 指定文件路径
-source_file = "/Users/bruce/code2024summer/python-react/openecel/target.xlsx"
-new_file = "/Users/bruce/code2024summer/python-react/openecel/source_first_70_rows.xlsx"
+source_file = "origin/source.xlsx"
+new_file = "source.xlsx" 
 
 # 检查文件是否存在
 if not os.path.exists(source_file):
     print(f"文件 {source_file} 不存在，请检查文件路径")
 else:
     try:
-        # 加载现有的 Excel 文件
         wb = load_workbook(source_file)
         ws = wb.active
 
@@ -18,13 +16,13 @@ else:
         new_wb = Workbook()
         new_ws = new_wb.active
 
-        # 复制前 70 行的数据到新的工作簿
-        for row in ws.iter_rows(min_row=1, max_row=70):
+        # 复制前 250 行的数据到新的工作簿
+        for row in ws.iter_rows(min_row=1, max_row=250):
             for cell in row:
                 new_ws[cell.coordinate] = cell.value
 
-        # 保存新的 Excel 文件
+        # 保存新的 Excel 文件到根目录
         new_wb.save(new_file)
-        print(f"前 70 行的数据已保存到 {new_file}")
+        print(f"前 250 行的数据已保存到 {new_file}")
     except Exception as e:
         print(f"处理文件时出现错误: {e}")
