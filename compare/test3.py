afd12cda578075a86c287f6d8962cee44fad4120
bruce chen <aaaeeettt124@126.com>
2024-06-10 16:43:18 +0800

    new

--- compare/test3.py
+++ compare/test3.py
@@ -10,8 +10,8 @@ sheet_hand = wb_hand.worksheets[0]  # 第一个工作表
 sheet_target = wb_target.worksheets[1]  # 第二个工作表
 
 # 获取数据
-data_hand = {sheet_hand.cell(row=i, column=4).value: i for i in range(2, sheet_hand.max_row + 1)}
-data_target = {sheet_target.cell(row=i, column=5).value: i for i in range(2, sheet_target.max_row + 1)}
+data_hand = {sheet_hand.cell(row=i, column=5).value: i for i in range(4, sheet_hand.max_row + 1)}
+data_target = {sheet_target.cell(row=i, column=4).value: i for i in range(2, sheet_target.max_row + 1)}
 
 # 创建一个新的工作簿和工作表
 wb_output = openpyxl.Workbook()
@@ -21,15 +21,21 @@ sheet_output = wb_output.active
 orange_fill = PatternFill(start_color="FFD580", end_color="FFD580", fill_type="solid")
 blue_fill = PatternFill(start_color="ADD8E6", end_color="ADD8E6", fill_type="solid")
 
-# 设置第一行标题，舍弃 hand 表的 C 列
-output_col = 1
-for col in range(1, sheet_hand.max_column + 1):
-    if col != 3:  # 跳过 C 列
-        sheet_output.cell(row=1, column=output_col, value=sheet_hand.cell(row=1, column=col).value)
-        output_col += 1
+# 设置第一行标题，使用 target 表的标题
+for col in range(1, sheet_target.max_column + 1):
+    sheet_output.cell(row=1, column=col, value=sheet_target.cell(row=1, column=col).value)
+
+# 写入表 target 中存在但表 hand 中不存在的行
+output_row = 2  # 从第二行开始写入数据
+for value_target, row_target in data_target.items():
+    if value_target not in data_hand:
+        for col in range(1, sheet_target.max_column + 1):
+            cell = sheet_target.cell(row=row_target, column=col)
+            new_cell = sheet_output.cell(row=output_row, column=col, value=cell.value)
+            new_cell.fill = blue_fill
+        output_row += 1
 
 # 写入表 hand 中存在但表 target 中不存在的行，舍弃 hand 表的 C 列
-output_row = 2  # 从第二行开始写入数据
 for value_hand, row_hand in data_hand.items():
     if value_hand not in data_target:
         output_col = 1
@@ -41,16 +47,7 @@ for value_hand, row_hand in data_hand.items():
                 output_col += 1
         output_row += 1
 
-# 写入表 target 中存在但表 hand 中不存在的行
-for value_target, row_target in data_target.items():
-    if value_target not in data_hand:
-        for col in range(1, sheet_target.max_column + 1):
-            cell = sheet_target.cell(row=row_target, column=col)
-            new_cell = sheet_output.cell(row=output_row, column=col, value=cell.value)
-            new_cell.fill = blue_fill
-        output_row += 1
-
 # 保存输出文件
-wb_output.save('output2.xlsx')
+wb_output.save('output3.xlsx')
 
-print("Comparison completed. Results saved in 'output.xlsx'.")
+print("Comparison completed. Results saved in 'output3.xlsx'.")
