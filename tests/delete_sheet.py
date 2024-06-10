10144dcfdf4fcbfe292452a216d4f5115823ddab
Brucey77 <aaaeeettt124@126.com>
2024-06-10 21:22:27 +0800

    excel

--- tests/delete_sheet.py
+++ tests/delete_sheet.py
@@ -3,13 +3,15 @@ import openpyxl
 # 打开Excel文件
 wb = openpyxl.load_workbook('target.xlsx')
 
+file_delete="try4"
+
 # 检查工作表是否存在
-if 'try4' in wb.sheetnames:
+if file_delete in wb.sheetnames:
     # 删除工作表
-    ws = wb['try4']
+    ws = wb[file_delete]
     wb.remove(ws)
     # 保存更改
     wb.save('target.xlsx')
-    print("工作表 'try3' 已被删除。")
+    print("工作表 {} 已被删除。".format(file_delete))
 else:
-    print("工作表 'try3' 不存在。")
+    print("工作表 {}不存在。".format(file_delete))
